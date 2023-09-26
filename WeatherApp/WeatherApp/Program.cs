0bf9a9ef5e6e604de4c0c8e0d9cad330735117ad
johanssonjulis <julia.johansson@helo.se>
2023-09-26 16:02:54 +0200

    Created program.cs

---
+++ WeatherApp/WeatherApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var userURL = ($"");
+
+            Console.WriteLine("Please input your API-key: ");
+            var apiKey = Console.ReadLine();
+
+            Console.WriteLine("Please input you cityname: ");
+            var cityName = Console.ReadLine().ToLower();
+
+            var getWeatherResult = client.GetStringAsync(userURL).Result;
+
+            
+
+
+
+        }
+    }
+}
